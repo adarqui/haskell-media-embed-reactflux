962f570f33724fac775db9c145b13c24d72bb44a
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-24 16:51:43 -0400

    in the works .. we will render youtube first

--- src/Web/Media/Embed/ReactFlux.hs
+++ src/Web/Media/Embed/ReactFlux.hs
@@ -1,2 +1,44 @@
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Web.Media.Embed.ReactFlux (
+  renderYoutube
 ) where
+
+
+
+import           Data.Monoid        ((<>))
+import           Data.Text          (Text)
+import qualified Data.Text          as Text
+import           React.Flux
+
+import           Web.Media.Embed
+
+
+
+#ifdef __GHCJS__
+import           Data.JSString      (JSString)
+import qualified Data.JSString.Text as JSS (textToJSString)
+#else
+type JSString = String
+#endif
+
+
+
+#ifdef __GHCJS__
+textToJSString' :: Text -> JSString
+textToJSString' = JSS.textToJSString
+#else
+textToJSString' :: Text -> String
+textToJSString' = Text.unpack
+#endif
+
+
+
+type HTMLView_ = ReactElementM ViewEventHandler ()
+
+
+
+
+renderYoutube :: YoutubeEmbed -> HTMLView_
+renderYoutube _ = undefined
